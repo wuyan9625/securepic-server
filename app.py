58c5c76685ccd486da3014b1caac3743a8ce1cf5
wuyan9625 <wuyan9625@gmail.com>
2025-03-28 21:57:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
