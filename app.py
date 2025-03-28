31e2d6eec16068a8d14cda367845b751c48cf031
wuyan9625 <wuyan9625@gmail.com>
2025-03-28 22:05:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, render_template, redirect, url_for, send_from_directory
 import os
 from utils import generate_id
+import logging
+
+# 設置日誌
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'static/uploads'
@@ -18,9 +22,17 @@ def upload():
         if file:
             file_id = generate_id()
             filename = file_id + '.jpg'
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
             view_url = f'/view?id={file_id}'
+
+            # 記錄上傳的圖片資訊
+            app.logger.debug(f"Received file: {filename}")
+            app.logger.debug(f"File saved at: {file_path}")
+            
             return render_template('upload_success.html', view_url=view_url)
+        else:
+            app.logger.debug("No file received")
     return render_template('upload.html')
 
 @app.route('/view')
@@ -36,4 +48,5 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
+    # 確保 Flask 接受外部請求
     app.run(host='0.0.0.0', port=5000, debug=True)
